29d57719aca59863fd4af62cf2dddba8690c97fd
Mark Reed <markdreed512@gmail.com>
2019-05-13 22:01:09 -0500

    Updates

---
+++ precache-manifest.70031f084b5992ad892596b5d57d3459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa648a341a119e11f58ad346525d95a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "dcd5de3ae0506f68c2d2",
+    "url": "/my-app/static/css/main.b0979ee8.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/my-app/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "dcd5de3ae0506f68c2d2",
+    "url": "/my-app/static/js/main.ac4520eb.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
